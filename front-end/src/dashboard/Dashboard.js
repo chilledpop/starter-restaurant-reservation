2a39268c5ac3901478dcb08d87ffca79e58942a8
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-05-26 01:34:43 -0700

    userstory01: implemented dashboard feature for listing all reservations for one date

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { previous, today, next } from "../utils/date-time";
+import useQuery from "../utils/useQuery";
+import ReservationList from "./ReservationList";
 
 /**
  * Defines the dashboard page.
@@ -8,7 +11,10 @@ import ErrorAlert from "../layout/ErrorAlert";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard({ date }) {
+function Dashboard() {
+  const query = useQuery();
+
+  const [date, setDate] = useState(query.get("date") || today());
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
@@ -23,14 +29,36 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  const handleChange = ({ target }) => {
+    setDate(target.value);
+  }
+
   return (
     <main>
       <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <div>
+        <button type="button" className="btn btn-info" onClick={() => setDate(previous(date))}>Previous</button>
+        <button type="button" className="btn btn-info" onClick={() => setDate(today())}>Today</button>
+        <button type="button" className="btn btn-info" onClick={() => setDate(next(date))}>Next</button>
       </div>
+      <br/>
+      <label htmlFor="reservation_date">
+        <input
+          id="reservation_date"
+          name="reservation_date"
+          type="date"
+          value={date}
+          onChange={handleChange}
+        />
+      </label>
+      <br/>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <div className="d-md-flex mb-3">
+        {reservations.length ? <h5>Reservations</h5> : `There are no reservations for ${date}.`}
+      </div>
+      {reservations.map((reservation) => (
+        <ReservationList key={reservation.reservation_id} reservation={reservation} />
+      ))}
     </main>
   );
 }
