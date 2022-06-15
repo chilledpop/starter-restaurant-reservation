38c9699610bad3118f65a84d890895c4cf700e59
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-06-15 01:15:15 -0700

    exported update function to reservations router and implemented PUT endpoint

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -12,16 +12,17 @@ router
   .route("/")
   .post(controller.create)
   .get(controller.list)
-  .all(methodNotAllowed)
-
-router
-  .route("/:reservation_id")
-  .get(controller.read)
-  .all(methodNotAllowed)
+  .all(methodNotAllowed);
 
 router
   .route("/:reservation_id/status")
   .put(controller.updateResStatus)
+  .all(methodNotAllowed);
+
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.update)
   .all(methodNotAllowed);
 
 module.exports = router;
