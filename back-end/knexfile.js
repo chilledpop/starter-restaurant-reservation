467fbb19c397fd0eaad6a78a55a5d30254387c9e
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-06-16 21:57:18 -0700

    hardcoded backend url to API_BASE_URL in api folder, removed hardcoded database url in knexfile file

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgres://mlissjbl:PseLxcdMN-jev4PxhDDBgHOMAA5Cmdg5@drona.db.elephantsql.com/mlissjbl",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
