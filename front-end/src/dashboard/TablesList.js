cca67a291fce5690f2d6ce3dc2efc362ccc23c30
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-06-19 19:07:29 -0700

    edited column media queries

--- front-end/src/dashboard/TablesList.js
+++ front-end/src/dashboard/TablesList.js
@@ -4,7 +4,7 @@ import React from "react";
 function TablesList({ table, handleFinish }) {
   
   return (
-    <div className="card col-sm-9 col-md-8 col-lg-6 m-auto">
+    <div className="card col-sm-8 col-md-7 col-lg-6 m-auto">
       <div className="card-body">
         <h5 className="card-title">Table Name: {table.table_name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">ID: {table.table_id}</h6>
